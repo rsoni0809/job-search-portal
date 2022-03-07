eaf2a33899f49e1c8afe492b074eb3c125a5f6bd
rsoni0809 <rahulsoni.iiita@gmail.com>
2022-03-07 22:08:30 +0530

    React typescript jira style task board
    React typescript jira style task board

--- src/Job.js
+++ src/Job.js
@@ -1,43 +1,35 @@
-import React, { useState } from "react";
-import { Card, Badge, Button, Collapse } from "react-bootstrap";
-import ReactMarkdown from "react-markdown";
+import React, { useState } from 'react'
+import { Card, Badge, Button, Collapse } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
 
-export const Job = ({ job }) => {
-  const [open, setOpen] = useState(false);
+export default function Job({ job }) {
+  const [open, setOpen] = useState(false)
+
   return (
     <Card className="mb-3">
       <Card.Body>
         <div className="d-flex justify-content-between">
           <div>
             <Card.Title>
-              {job.title} -{" "}
-              <span className="text-muted font-weight-light">
-                {job.company}
-              </span>
+              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
             </Card.Title>
             <Card.Subtitle className="text-muted mb-2">
               {new Date(job.created_at).toLocaleDateString()}
             </Card.Subtitle>
-            <Badge variant="secondary">{job.type}</Badge>
+            <Badge variant="secondary" className="mr-2">{job.type}</Badge>
             <Badge variant="secondary">{job.location}</Badge>
-            <div style={{ wordBreak: "break-all" }}>
+            <div style={{ wordBreak: 'break-all' }}>
               <ReactMarkdown source={job.how_to_apply} />
             </div>
           </div>
-
-          <img
-            src={job.company_logo}
-            alt={job.company}
-            className="d-none d-md-block "
-            height="50"
-          />
+          <img className="d-none d-md-block" height="50" alt={job.company} src={job.company_logo} />
         </div>
         <Card.Text>
           <Button
-            variant="primary"
             onClick={() => setOpen(prevOpen => !prevOpen)}
+            variant="primary"
           >
-            details
+            {open ? 'Hide Details' : 'View Details'}
           </Button>
         </Card.Text>
         <Collapse in={open}>
@@ -47,5 +39,5 @@ export const Job = ({ job }) => {
         </Collapse>
       </Card.Body>
     </Card>
-  );
-};
+  )
+}
